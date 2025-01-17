966f2d5229c7de1520ccf43c85b939dc91062900
avegap23 <avpampliega@gmail.com>
2025-01-17 13:14:12 +0100

    render

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,30 @@ import './App.css'
 function App() {
   const wishlist = [
     { id: 1, name: "PlayStation 5", done: false },
-    { id: 2, name: "Macbook Pro", done: false },
+    { id: 2, name: "Macbook Pro", done: true },
     { id: 3, name: "Tesla Model S", done: false }
   ]
 
   return (
     <>
       <h1>My wishlist</h1>
-      <fieldset id="new wishes">
-        <p><input type="text" placeholder="Introduce tu deseo" /></p>
+      <fieldset id="new wishes" className="wish-input">
+        <p><input type="text" placeholder="Introduce tu deseo" className='wish-input__field' /></p>
         <p><button>Add wish</button></p>
-        <legend>Adding new wishes</legend>
+        <legend className="wish-input__label">Adding new wishes</legend>
       </fieldset>
       <div id="list">
-        <ul>
+        <ul className="wish-list">
           {wishlist.map(({ id, name, done }) => (
-            <li key={id}>
+            <li key={id} className={`wish-list__item + ${done ? "wish-list__item--done" : ""}`}>
               <input type="checkbox" defaultChecked={done} id={id} />
-              <label htmlFor={id}>{name}</label>
+              <label htmlFor={id} className={done ? "wish-list__item--done label" : ""}>{name}</label>
+              {/* wish-list__item--done */}
             </li>
           ))}
         </ul>
-        <button>Achive done</button>
-      </div>
+        <button className="wish-clear">Achive done</button>
+      </div >
     </>
   )
 }
